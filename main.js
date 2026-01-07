385969764aae8838abeb228d999b2cf446fce6c6
Nico de la Fuente <ndelafuente@bionanogenomics.com>
2026-01-07 11:49:47 -0800

    testing Safari datalist separate option for translation

--- main.js
+++ main.js
@@ -104,11 +104,14 @@ if (VALIDATE_RECIPES) {
         const recipeNames = Object.keys(recipes).sort((a, b) => normalize(a).localeCompare(normalize(b)));
         for (const recipe of recipeNames) {
             const opt = document.createElement("option");
-            // opt.value = recipes[recipe].recipe;
-            if ('translation' in recipes[recipe]) {
-                opt.text = recipes[recipe].translation?.normalize('NFKD');
-            }
+            opt.value = recipes[recipe].recipe;
             dl.appendChild(opt);
+            if ('translation' in recipes[recipe]) {
+                const optTr = document.createElement("option");
+                optTr.value = recipes[recipe].recipe;
+                optTr.text = recipes[recipe].translation?.normalize('NFKD').replace(/([a-z])[^ a-z]+/ig, '$1');
+                dl.appendChild(optTr);
+            }
         };
     }
 
